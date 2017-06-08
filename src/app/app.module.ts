3ca92e8956f7874e0260190944995c3af35add73
Brendon <brendongirao@gmail.com>
2017-06-08 11:11:54 -0300

    tela principal artesão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormCadastro1Component } from './form-cadastro-1/form-cadastro-1.component';
+import { CardPedidoGrandeComponent } from './card-pedido-grande/card-pedido-grande.component'
 
-import { CadastroService } from './form-cadastro-1/form-cadastro-1.service'
+import { CadastroService } from './form-cadastro-1/form-cadastro-1.service';
+import { TelaPrincipalArtesaoComponent } from './tela-principal-artesao/tela-principal-artesao.component';
+import { CardPedidoPequenoComponent } from './card-pedido-pequeno/card-pedido-pequeno.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    FormCadastro1Component
+    FormCadastro1Component,
+    CardPedidoGrandeComponent,
+    TelaPrincipalArtesaoComponent,
+    CardPedidoPequenoComponent
   ],
   imports: [
     BrowserModule,
