3fa35dc69b1e0c674151334e3200e688f3eee855
unknown <macelocosta@hotmail.com>
2017-06-21 20:35:48 -0300

    correcoes nos posicionamentos

--- src/app/detalhe-produto/detalhe-produto.component.ts
+++ src/app/detalhe-produto/detalhe-produto.component.ts
@@ -11,6 +11,9 @@ export class DetalheProdutoComponent implements OnInit {
   selectedImage;
   showFullscreen:boolean = false;
   product;
+  anotherAuthorProducts;
+  productSuggestions;
+  newComment:string;
   comments;
 
   setSelectedImage(image){
@@ -31,13 +34,27 @@ export class DetalheProdutoComponent implements OnInit {
     ];
 
     this.product = {title:'Enfeite para parede "Telhas pintadas com as belezas de Quixadá"',
-                    price:'R$ 25,73',
+                    price:'24.99',
                     units:50,
                     size:'40cm',
                     materials:'barro',
                     description:'Enfeite para parede "Telhas pintadas com as belezas de Quixadá" - nas cores lilás, vermelho e amarelo. Enfeite para parede "Telhas pintadas com as belezas de Quixadá" - nas cores lilás, vermelho e amarelo. Enfeite para parede "Telhas pintadas com as belezas de Quixadá" - nas cores lilás, vermelho e amarelo.',
                     authorBio:'Meu nome é Joana e trabalho especificamente com produtos feitos de barro.'};
 
+    this.anotherAuthorProducts = [
+      {'imageUrl':'assets/images/components/detalhe-produto/more-01.jpg'},
+      {'imageUrl':'assets/images/components/detalhe-produto/more-02.jpg'},
+      {'imageUrl':'assets/images/components/detalhe-produto/more-03.jpg'},
+      {'imageUrl':'assets/images/components/detalhe-produto/more-04.jpg'}
+    ];
+
+    this.productSuggestions = [
+      {'imageUrl':'assets/images/components/detalhe-produto/suggestion-01.jpg'},
+      {'imageUrl':'assets/images/components/detalhe-produto/suggestion-02.jpg'},
+      {'imageUrl':'assets/images/components/detalhe-produto/suggestion-03.jpg'},
+      {'imageUrl':'assets/images/components/detalhe-produto/suggestion-04.jpg'}
+    ];
+    
     this.comments = [
       {"author":"Maria", "text":"Produto de ótima qualidade! Recomendo!"},
       {"author":"José", "text":"Chegou direitinho, estou bastante satisfeito!"},
